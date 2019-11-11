4f515cd8ce777f0324a2d87ba28e59d32f809398
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-11 13:42:24 +0000

    create router for users

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet')
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
+const userRouter = require('./users/usersRouter')
 
 const server = express();
 server.use(helmet());
@@ -9,5 +10,7 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.json('Its working!')
 })
+
+server.use('/api/users', userRouter);
 
 module.exports = server;
