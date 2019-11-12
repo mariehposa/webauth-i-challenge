89639b8f93270389129551f2afeb552ce0dc587d
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-11-12 15:26:31 +0000

    MVP completed

--- users/usersRouter.js
+++ users/usersRouter.js
@@ -3,17 +3,31 @@ const bcrypt = require('bcrypt')
 const db = require('./usersModel')
 const router = express.Router()
 
-router.get('/', bodyValidator, (req, res) => {
+router.get('/', restricted, (req, res) => {
     db.getUsers()
-    .then(users => {
-        res.status(200).json(users)
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: "Couldn't get all users" + err.message
+        .then(users => {
+            res.status(200).json(users)
         })
-    })
+        .catch(err => {
+            res.status(500).json({
+                message: "Couldn't get all users" + err.message
+            })
+        })
 })
+
+router.get('/logout', (req, res) => {
+    if (req.session) {
+      req.session.destroy(err => {
+        if (err) {
+          res.json('you can not leave, actually')
+        } else {
+          res.json('goodbye, sad to see you go')
+        }
+      })
+    } else {
+      res.end();
+    }
+  })
 
 router.post('/register', (req, res) => {
     let hash = bcrypt.hashSync(req.body.password, 12)
@@ -23,38 +37,42 @@ router.post('/register', (req, res) => {
     };
 
     db.addUsers(newUser)
-    .then(user => {
-        res.status(201).json(user)
-    })
-    .catch(error => {
-        res.status(500).json(error.message);
-    });
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(error => {
+            res.status(500).json(error.message);
+        });
 })
 
-router.post('/login', bodyValidator, (req, res) => {
-    res.status(200).json({ message: `Welcome ${req.valUser.name} !`})
-})
-
-function bodyValidator (req, res, next) {
+router.post('/login', (req, res) => {
     let { name, password } = req.body;
 
-    if (!name || !password) {
-        name = req.headers.name
-        password = req.headers.password
-    }
 
     db.getUserBy({ name })
-    .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-            req.valUser = user
-            next()
-        } else {
-            res.status(401).json({ message: 'Invalid Credentials!'})
-        }
-    })
-    .catch(err => {
-        res.status(500).json(err.message)
-    })
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                req.session.user = user;
+                res.status(200).json({ message: `Welcome ${name} !` })
+
+            } else {
+                res.status(401).json({ message: 'Invalid Credentials!' })
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err.message)
+        })
+    // res.status(200).json({ message: `Welcome ${name} !` })
+})
+
+function restricted (req, res, next) {
+   if (req.session && req.session.user ) {
+       next()
+   } else {
+       res.status(401).json({
+           message: 'User is unauthorized'
+       })
+   }
 }
 
 module.exports = router;
